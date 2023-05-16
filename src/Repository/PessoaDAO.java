438988b6727d4ab832f9b214290033c057fc9476
guilherme.peritto <guilherme.perito@useall.com.br>
2023-05-16 10:23:50 -0300

    Criacao da classe Pessoa com o DAO e o menu principal

--- src/Repository/PessoaDAO.java
+++ src/Repository/PessoaDAO.java
@@ -1,5 +1,14 @@
 package Repository;
+import Model.Pessoa;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PessoaDAO {
-
+    static List<Pessoa> pessoas = new ArrayList<>();
+    public static void salvar(Pessoa pessoa) {
+        pessoas.add(pessoa);
+    }
+    public static List<Pessoa> buscaTodos() {
+            return pessoas;
+        }
 }
