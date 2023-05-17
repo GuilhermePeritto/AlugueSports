fc8a96577495d25cbd639b154f67cfeef5d13d50
Wesley Santos <116184309+wesleyesantos1998@users.noreply.github.com>
2023-05-17 20:06:15 -0300

    Foi criado os atributos Codigo, NomeCidade e Estado, contrutores, Get/Setters, ToString.

---
+++ src/Model/Cidade.java
@@ -0,0 +1,48 @@
+package Model;
+
+public class Cidade {
+    Integer Codigo;
+
+    String NomeCidade;
+    Estado estado;
+
+    public Cidade(Integer codigo, String nomeCidade, Estado estado) {
+        Codigo = codigo;
+        NomeCidade = nomeCidade;
+        this.estado = estado;
+    }
+
+    public Integer getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        Codigo = codigo;
+    }
+
+    public String getNomeCidade() {
+        return NomeCidade;
+    }
+
+
+    public void setNomeCidade(String nomeCidade) {
+        NomeCidade = nomeCidade;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidade{" +
+                "Codigo=" + Codigo +
+                ", NomeCidade='" + NomeCidade + '\'' +
+                ", estado=" + estado +
+                '}';
+    }
+}
