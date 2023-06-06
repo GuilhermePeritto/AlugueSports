d29033299d2f03049cb490fef6c378d80b71b42d
Wesley Santos <116184309+wesleyesantos1998@users.noreply.github.com>
2023-06-05 21:55:24 -0300

    Foi adicionado nos processos, a parte de Alterar Pais e Excluir Pais.

--- src/View/ViewController.java
+++ src/View/ViewController.java
@@ -392,29 +392,6 @@ public class ViewController extends View {
 
     }
 
-    private static void processoPais() {
-        Object[] selectionValues = PaisDAO.findPaisInArray();
-        String initialSelection = (String) selectionValues[0];
-        Object selection = JOptionPane.showInputDialog(null, "Selecione o cliente!",
-                "Processo", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
-        List<Pais> pais = PaisDAO.buscarPorNome((String) selection);
-        verificaRegistroNullo(selection);
-        Object[] selectionValuesCliente = {"Alterar Dados", "Excluir Cadastro"};
-        String initialSelectionCliente = (String) selectionValues[0];
-        Object selectionCliente = JOptionPane.showInputDialog(null, "Selecione o processo!",
-                "Processo", JOptionPane.QUESTION_MESSAGE, null, selectionValuesCliente, initialSelectionCliente);
-        verificaRegistroNullo(selectionCliente);
-
-        switch ((String) selectionCliente) {
-            case "Alterar Dados":
-                PaisDAO.alterardadospais(pais.get(0));
-                break;
-            case "Excluir Cadastro":
-                PaisDAO.excluirCadastropais(pais.get(0));
-        }
-
-    }
-
     public static void processoCliente() {
         Object[] selectionValues = ClienteDAO.findUsuarioClienteInArray();
         String initialSelection = (String) selectionValues[0];
