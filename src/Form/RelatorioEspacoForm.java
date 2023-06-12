34db8e7263b044ff77a647572fdf1781f96151e7
rafachagas <54208472+rafachagas@users.noreply.github.com>
2023-06-12 18:41:47 -0300

    Criação do relatório de espaço

---
+++ src/Form/RelatorioEspacoForm.java
@@ -0,0 +1,70 @@
+package Form;
+
+import Model.Espaco;
+import Relatorio.TableEspaco;
+import javax.swing.*;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+import java.util.Vector;
+
+import static View.ViewController.chamaMenuRelatorios;
+
+public class RelatorioEspacoForm extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String[] nomeColunas =
+            {"Codigo", "Nome", "Status"};
+
+    protected JTable table;
+    protected JScrollPane scroller;
+    protected TableEspaco tabela;
+
+    public RelatorioEspacoForm(Vector<Espaco> vetorDados) {
+        iniciarComponentes(vetorDados);
+    }
+
+    public void iniciarComponentes(Vector<Espaco> vetorDados) {
+        tabela = new TableEspaco(nomeColunas, vetorDados);
+        table = new JTable();
+        table.setModel(tabela);
+        table.setSurrendersFocusOnKeystroke(true);
+        scroller = new javax.swing.JScrollPane(table);
+        table.setPreferredScrollableViewportSize(new java.awt.Dimension(500, 300));
+
+        TableColumn colunaEscondida = table.getColumnModel().getColumn(TableEspaco.INDEX_ESCONDIDO);
+        colunaEscondida.setMinWidth(2);
+        colunaEscondida.setPreferredWidth(2);
+        colunaEscondida.setMaxWidth(2);
+        setLayout(new BorderLayout());
+        add(scroller, BorderLayout.CENTER);
+    }
+
+    public static void emitirRelatorio(List<Espaco> espacos) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            JFrame frame = new JFrame("Relatorio");
+
+            frame.addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent evt) {
+                    frame.setVisible(false);
+                    chamaMenuRelatorios();
+                }
+            });
+            Vector<Espaco> vetorDados = new Vector<Espaco>();
+            for (Espaco espaco : espacos) {
+                vetorDados.add(espaco);
+            }
+
+            frame.getContentPane().add(new RelatorioEspacoForm(vetorDados));
+            frame.pack();
+            frame.setVisible(true);
+            frame.setLocationRelativeTo(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
